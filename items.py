d3c3b105dca181728c428aac74b48624cfb7b628
lgo20 <91659775+lgo20@users.noreply.github.com>
2022-10-17 16:33:44 +0100

    Update items.py

--- items.py
+++ items.py
@@ -3,6 +3,7 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+import re
 import scrapy
 from scrapy.loader import ItemLoader
 from itemloaders.processors import TakeFirst, MapCompose
@@ -15,12 +16,20 @@ from w3lib.html import (
 import datetime
 
 
-def clear(value):
-    if isinstance(value, str):
-        value = value.replace("\xa0", " ")
-        value = value.strip()
+def decode_unicode(value):
+    if value is not None:
+        value = value.encode('ascii', 'ignore')
+        value = value.decode()
     return value
 
+def list_str(value):
+    if isinstance(value, list):
+        vals = ""
+        for i in value:
+            vals += f" {i}"
+        return vals.strip()
+
+    return value
 
 def date(value):
     if isinstance(value, datetime.date):
@@ -28,48 +37,57 @@ def date(value):
         value = value.strftime("%Y-%m-%d")
     return value
 
+def strip_spaces(value):
+    return value.strip()
+
+def website(value):
+    match_obj = re.search("www\..*\.[a-z]{3}", value)
+    if match_obj:
+        return match_obj.group(0)
 
 class JobsItem(scrapy.Item):
-    id = scrapy.Field(output_processor=TakeFirst())
+    id = scrapy.Field(input_processor=MapCompose(remove_tags, replace_escape_chars), output_processor=TakeFirst())
  #url for the job advert itself
     url = scrapy.Field(output_processor=TakeFirst())
  #the job title
     title = scrapy.Field(
-        input_processor=MapCompose(remove_tags, replace_entities, clear),
+        input_processor=MapCompose(remove_tags, replace_entities, replace_escape_chars, strip_spaces, decode_unicode),
         output_processor=TakeFirst(),
     )
 #the link on the organisation's website which takes the user to the application process. Eg often an 'apply here' button 
     application_url = scrapy.Field(output_processor=TakeFirst())
     date_created = scrapy.Field(
-        input_processor=MapCompose(date), output_processor=TakeFirst()
+        input_processor=MapCompose(replace_escape_chars), output_processor=TakeFirst()
     )
     date_closing = scrapy.Field(
-        input_processor=MapCompose(date), output_processor=TakeFirst()
+        input_processor=MapCompose(date, replace_escape_chars, remove_tags), 
+        output_processor=TakeFirst()
     )
  #the bulk description of the role - ie what it consists of, skills required, details of the position. Scrape in html format
     description = scrapy.Field(
-        input_processor=MapCompose(remove_tags, replace_entities, clear),
+        input_processor=MapCompose(replace_escape_chars, decode_unicode),
         output_processor=TakeFirst(),
     )
   #specific to categories on intdevjob.com. Often will not be possible to scrape
     category = scrapy.Field(
-        input_processor=MapCompose(remove_tags, clear), output_processor=TakeFirst()
+        input_processor=MapCompose(remove_tags, replace_escape_chars, decode_unicode), output_processor=TakeFirst()
     )
     location = scrapy.Field(
-        input_processor=MapCompose(clear), output_processor=TakeFirst()
+        input_processor=MapCompose(list_str, remove_tags, replace_escape_chars),
+        output_processor=TakeFirst()
     )
     #full time, part time, etc. See https://app.jboard.io/api/documentation
     contract_type = scrapy.Field(
-        input_processor=MapCompose(remove_tags, clear), output_processor=TakeFirst()
+        input_processor=MapCompose(remove_tags, replace_escape_chars, decode_unicode), output_processor=TakeFirst()
     )
     contract_type_jboard = scrapy.Field(output_processor=TakeFirst())
     salary = scrapy.Field(
-        input_processor=MapCompose(clear), output_processor=TakeFirst()
+        input_processor=MapCompose(decode_unicode), output_processor=TakeFirst()
     )
     #name of the organisation
-    organisation = scrapy.Field(output_processor=TakeFirst())
+    organisation = scrapy.Field(input_processor=MapCompose(remove_tags, replace_escape_chars, decode_unicode), output_processor=TakeFirst())
     #organisation's website
-    website = scrapy.Field(output_processor=TakeFirst())
+    website = scrapy.Field(input_processor=MapCompose(remove_tags, replace_escape_chars, decode_unicode, website), output_processor=TakeFirst())
     # wp_logo = scrapy.Field(output_processor=TakeFirst())
     #jboard allocated no.
     employer_id_jboard = scrapy.Field(output_processor=TakeFirst())
